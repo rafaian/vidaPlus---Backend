7681e4f54f2cc72ff1ca3c9e9696a91db0e36000
rafaian <rafaelserafimx@gmail.com>
2026-05-17 23:19:58 -0300

    Sistema de autenticação JWT funcionando

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
-
+const jwt = require("jsonwebtoken")
 const router = express.Router()
 
 const conectarBanco = require("../database/database")
@@ -56,7 +56,55 @@ router.post("/usuarios", async (req, res) => {
     res.status(201).json({
         mensagem: `Usuário ${nome} cadastrado com sucesso!`
     })
+})
+
+// Login:
+router.post("/login", async (req, res) => {
+
+    const db = await conectarBanco()
+
+    const { email , senha } = req.body
+
+    // Buscar usúario:
+    const usuario = await db.get(
+        "SELECT * FROM usuarios WHERE email = ?",
+        [email]
+    )
+    if (!usuario) {
+        return res.status(400).json({
+            erro: "Email ou senha inválidos"
+        })
+    }    
+
+    // Comparar senha:
+    const senhaCorreta = await bcrypt.compare(
+        senha,
+        usuario.senha
+    )
+    if(!senhaCorreta) {
+        return res.status(400).jason({
+            erro: "Email ou senha inválidos"
+        })
+    }
+    // Gerando Token:
+    const token = jwt.sign(
+        {
+            id: usuario.id,
+            email: usuario.email
+        },
+    
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+)
+
+res.json({
+    mensagem: "Login realizado com sucesso!",
+    token
+})
 
 })
+
 
 module.exports = router
